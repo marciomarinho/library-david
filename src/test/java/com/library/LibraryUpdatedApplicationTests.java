866bd7a9f118ae23e8fa72f907b977b6f8e3d8fb
marciomarinho <marciomarinho@gmail.com>
2016-03-20 18:01:34 +1100

    Adding travis-ci information on README.md

--- src/test/java/com/library/LibraryUpdatedApplicationTests.java
+++ src/test/java/com/library/LibraryUpdatedApplicationTests.java
@@ -1,18 +1,46 @@
 package com.library;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+//import static org.hamcrest.Matchers.hasProperty;
+//import static org.hamcrest.Matchers.nullValue;
+//import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = LibraryUpdatedApplication.class)
 @WebAppConfiguration
 public class LibraryUpdatedApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private MockMvc mockMvc;
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void showAddTodoForm() throws Exception {
+        mockMvc.perform(get("/book"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book/book"));
+//                .andExpect(model().attribute("books", hasProperty("id", nullValue())))
+//                .andExpect(model().attribute("books", hasProperty("description", isEmptyOrNullString())))
+//                .andExpect(model().attribute("books", hasProperty("title", isEmptyOrNullString())));
+    }
 }
